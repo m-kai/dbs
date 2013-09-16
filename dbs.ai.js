09b2fa5658cad9fac0fd2000f7e2ab009293a4bd
m-kai <esper2000@gmail.com>
2013-09-16 21:35:09 +0900

    標準探索アルゴリズムをスーパークラスに用意

--- dbs.ai.js
+++ dbs.ai.js
@@ -7,10 +7,67 @@ function AI() {
 
 AI.prototype = {
   memo : "", // viewに表示したりする
+  maxDepth : 3, // alphaBeta探索時の最大探索深度
 
   // 探索を実行し結果(hand)を返す
   execute : function(banmen, isBlackTurn) {
-    return new Hand()
+    var hands = banmen.createLegalHands(isBlackTurn)
+    var maxId = 0
+    var maxValue = -Infinity
+    var minId = 0
+    var minValue = Infinity
+    this.memo = ""
+    for(var i = 0;i < hands.length;i++) {
+      var v = this.alphaBeta(
+        banmen.execute(hands[i]), !isBlackTurn, this.maxDepth,
+        -Infinity, Infinity
+      )
+      if(maxValue < v){
+        maxId = i
+        maxValue = v
+      } else if(v < minValue) {
+        minId = i
+        minValue = v
+      }
+      this.memo = this.memo + hands[i].from + "->" + hands[i].to + " : " + v + "\n"
+    }
+    return hands[isBlackTurn ? maxId : minId]
+  },
+
+  // 標準探索アルゴリズム（αβカット）
+  alphaBeta : function(cban, isBlackTurn, depth, a, b) {
+    var eg
+    if(eg = cban.checkEndGame()) {
+      // 終局まで探索
+      if(eg == BLACK_WIN) return Infinity
+      if(eg == WHITE_WIN) return -Infinity
+      return 0
+    } else if(depth == 0) {
+      // 探索打ち切り。評価関数による局面評価
+      return this.evaluate(cban, isBlackTurn)
+    }
+    var hands = cban.createLegalHands(isBlackTurn)
+    if(isBlackTurn) {
+      for(var i = 0;i < hands.length;i++) {
+        a = Math.max(a, this.alphaBeta(cban.execute(hands[i]), !isBlackTurn, depth - 1, a, b))
+        if(b <= a){
+          return b // βカット
+        }
+      }
+      return a
+    } else {
+      for(var i = 0;i < hands.length;i++) {
+        b = Math.min(b, this.alphaBeta(cban.execute(hands[i]), !isBlackTurn, depth - 1, a, b))
+        if(b <= a){
+          return a // αカット
+        }
+      }
+      return b
+    }
+  },
+
+  evaluate : function(cban, isBlackTurn) {
+    return 0;
   },
 }
 
@@ -33,7 +90,7 @@ MonteCarloAI.prototype.execute = function(banmen, isBlackTurn) {
         // 決着付くまでランダム試行する
         cban = banmen.execute(hands[i])
         cturn = isBlackTurn
-        while(cban.checkEndGame() == 0) {
+        while(!cban.checkEndGame()) {
           cturn = !cturn
           chands = cban.createLegalHands(cturn)
           ch = chands[Math.floor(Math.random() * chands.length)]
@@ -55,3 +112,28 @@ MonteCarloAI.prototype.execute = function(banmen, isBlackTurn) {
 
     return hands[maxId]
   }
+
+// AI:モンテカルロ法を動的評価関数として使うヤツ
+function MonteCarloSpecial(maxDepth, trials) {
+  this.maxDepth = maxDepth || 7
+  this.trials = trials || 10
+}
+
+MonteCarloSpecial.prototype = new AI()
+MonteCarloSpecial.prototype.evaluate = function(cban, isBlackTurn) {
+  result = [0, 0, 0, 0]
+  for(var j = 0;j < this.trials;j++) {
+    // 決着付くまでランダム試行する
+    tban = cban
+    cturn = isBlackTurn
+    while(!tban.checkEndGame()) {
+      cturn = !cturn
+      chands = tban.createLegalHands(cturn)
+      ch = chands[Math.floor(Math.random() * chands.length)]
+      tban = tban.execute(ch)
+    }
+    result[tban.checkEndGame()]++
+  }
+  return result[BLACK_WIN] - result[WHITE_WIN]
+}
+
